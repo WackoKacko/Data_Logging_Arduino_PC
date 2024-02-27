19f421522cbd25ca379762034f9094736a9cda08
Kacper Bardecki <kbardecki@gmail.com>
2024-02-27 10:51:33 +0000

    added yaml file and streamlined loop

--- frasers_controller/d_Loop.ino
+++ frasers_controller/d_Loop.ino
@@ -9,22 +9,18 @@ void loop() {
 
  //*NOTE: trying inverting "digitalWrite(..., HIGH)" and "digitalWrite(..., LOW)" if you are seeing unexpected behavior.
   rh_PID.Compute();
-  if (rh_output > WINDOW_SIZE-MIN_WINDOW) rh_output = WINDOW_SIZE; //quickest change 0.5s
-  else if (rh_output <= MIN_WINDOW) rh_output = 0; //quickest change 0.5s
+  // if (rh_output > WINDOW_SIZE-MIN_WINDOW) rh_output = WINDOW_SIZE; //quickest change 0.5s
+  // else if (rh_output <= MIN_WINDOW) rh_output = 0; //quickest change 0.5s
   if (millis() - rh_start > WINDOW_SIZE) rh_start += WINDOW_SIZE; //time to shift the Relay Window
   if (rh_output < millis() - rh_start) digitalWrite(SOLENOID_VALVE_RELAY_PIN, !HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
   else digitalWrite(SOLENOID_VALVE_RELAY_PIN, !LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
 
   ih_PID.Compute();
-  if (ih_output > WINDOW_SIZE-MIN_WINDOW) ih_output = WINDOW_SIZE; //can change no faster than MIN_WINDOW
-  else if (ih_output <= MIN_WINDOW) ih_output = 0; //can change no faster than WINDOW_SIZE
   if (millis() - ih_start > WINDOW_SIZE) ih_start += WINDOW_SIZE; //time to shift the Relay Window (ih_start = millis() also works)
   if (ih_output < millis() - ih_start) digitalWrite(IMMERSION_HEATER_RELAY_PIN, HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
   else digitalWrite(IMMERSION_HEATER_RELAY_PIN, LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
 
   bh_PID.Compute();
-  if (bh_output > WINDOW_SIZE-MIN_WINDOW) bh_output = WINDOW_SIZE; //quickest change 0.5s
-  else if (bh_output <= MIN_WINDOW) bh_output = 0; //quickest change 0.5s
   if (millis() - bh_start > WINDOW_SIZE) bh_start += WINDOW_SIZE; //time to shift the Relay Window
   if (bh_output < millis() - bh_start) digitalWrite(BOX_HEATER_RELAY_PIN, HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
   else digitalWrite(BOX_HEATER_RELAY_PIN, LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
