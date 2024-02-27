9cd6f7dbf66ec0d7e223db1db254644a6bba58d6
Kacper Bardecki <kbardecki@gmail.com>
2024-02-27 17:46:31 +0000

    Update d_Loop.ino

--- frasers_controller/d_Loop.ino
+++ frasers_controller/d_Loop.ino
@@ -7,23 +7,12 @@ void loop() {
 
   if ( millis() > WATCHDOG_TIMEOUT_PERIOD ) delay(5000); //software reset of Arduino using watchdog timer
 
- //*NOTE: trying inverting "digitalWrite(..., HIGH)" and "digitalWrite(..., LOW)" if you are seeing unexpected behavior.
-  rh_PID.Compute();
-  if (millis() - rh_start > WINDOW_SIZE) rh_start = millis();// WINDOW_SIZE; //time to shift the Relay Window
-  if (rh_output <= millis() - rh_start) {
-    if (digitalRead(SOLENOID_VALVE_RELAY_PIN) == HIGH && millis() - last_change >= MIN_CHANGE_TIME) {
-      digitalWrite(SOLENOID_VALVE_RELAY_PIN, !HIGH); //window off time (INVERTED LOGIC SOLENOID VS SSR)
-      last_change = millis(); // Update the time of the last change
-      // Serial.print("RH UP, with rh_output = "); Serial.println(rh_output);
-    }
+  if (millis() - rh_start > WINDOW_SIZE) {
+    rh_start = millis();// WINDOW_SIZE; //time to shift the Relay Window
+    rh_PID.Compute();
   }
-  else {
-    if (digitalRead(SOLENOID_VALVE_RELAY_PIN) == LOW && millis() - last_change >= MIN_CHANGE_TIME) {
-      digitalWrite(SOLENOID_VALVE_RELAY_PIN, !LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
-      last_change = millis(); // Update the time of the last change
-      // Serial.print("RH DOWN, with rh_output = "); Serial.println(rh_output);
-    }
-  }
+  if (rh_output < millis() - rh_start) digitalWrite(SOLENOID_VALVE_RELAY_PIN, !HIGH);
+  else digitalWrite(SOLENOID_VALVE_RELAY_PIN, !LOW);
 
   ih_PID.Compute();
   if (millis() - ih_start > WINDOW_SIZE) ih_start = millis(); //time to shift the Relay Window (ih_start = millis() also works)
