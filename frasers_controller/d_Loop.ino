cd8b9783d31f2e381b9b77637451ff8372f34b37
Kacper Bardecki <kbardecki@gmail.com>
2024-02-29 16:26:49 +0000

    Update d_Loop.ino

--- frasers_controller/d_Loop.ino
+++ frasers_controller/d_Loop.ino
@@ -7,21 +7,18 @@ void loop() {
 
   if ( millis() > WATCHDOG_TIMEOUT_PERIOD ) delay(5000); //software reset of Arduino using watchdog timer
 
- //*NOTE: trying inverting "digitalWrite(..., HIGH)" and "digitalWrite(..., LOW)" if you are seeing unexpected behavior.
   rh_PID.Compute();
-  // if (rh_output > WINDOW_SIZE-MIN_WINDOW) rh_output = WINDOW_SIZE; //quickest change 0.5s
-  // else if (rh_output <= MIN_WINDOW) rh_output = 0; //quickest change 0.5s
-  if (millis() - rh_start > WINDOW_SIZE) rh_start += WINDOW_SIZE; //time to shift the Relay Window
-  if (rh_output < millis() - rh_start) digitalWrite(SOLENOID_VALVE_RELAY_PIN, !HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
-  else digitalWrite(SOLENOID_VALVE_RELAY_PIN, !LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
+  if (millis() - rh_start > WINDOW_SIZE) rh_start = millis();// WINDOW_SIZE; //time to shift the Relay Window
+  if (rh_output > millis() - rh_start && rh_output > 500) digitalWrite(SOLENOID_VALVE_RELAY_PIN, HIGH); //NOTE THE >/<
+  else digitalWrite(SOLENOID_VALVE_RELAY_PIN, LOW);
 
   ih_PID.Compute();
-  if (millis() - ih_start > WINDOW_SIZE) ih_start += WINDOW_SIZE; //time to shift the Relay Window (ih_start = millis() also works)
+  if (millis() - ih_start > WINDOW_SIZE) ih_start = millis(); //time to shift the Relay Window (ih_start = millis() also works)
   if (ih_output < millis() - ih_start) digitalWrite(IMMERSION_HEATER_RELAY_PIN, HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
   else digitalWrite(IMMERSION_HEATER_RELAY_PIN, LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
 
   bh_PID.Compute();
-  if (millis() - bh_start > WINDOW_SIZE) bh_start += WINDOW_SIZE; //time to shift the Relay Window
+  if (millis() - bh_start > WINDOW_SIZE) bh_start = millis(); //time to shift the Relay Window
   if (bh_output < millis() - bh_start) digitalWrite(BOX_HEATER_RELAY_PIN, HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
   else digitalWrite(BOX_HEATER_RELAY_PIN, LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
 }
