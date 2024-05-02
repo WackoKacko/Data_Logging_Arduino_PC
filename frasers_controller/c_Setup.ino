f8aa17fc87bae0b65caf3b0db668ff7939182e2c
Kacper Bardecki <kbardecki@gmail.com>
2024-05-02 11:13:13 +0100

    cleanup

--- frasers_controller/c_Setup.ino
+++ frasers_controller/c_Setup.ino
@@ -46,28 +46,16 @@ void setup() {
 
 
   //BMP280 sensor start
-  // if(!bmp.begin()){ Serial.println("BMP init failed!"); /*while(1);*/ }
-  // else Serial.println("BMP init success!");
-    /* Default settings from datasheet. */
-      unsigned status;
-  //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
-  status = bmp.begin(0x76);
-  if (!status) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
-    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-    Serial.print("        ID of 0x60 represents a BME 280.\n");
-    Serial.print("        ID of 0x61 represents a BME 680.\n");
-    while (1) delay(10);
+  if (!bmp.begin(0x76)) Serial.println("BMP280 Failed!");
+  else {
+    Serial.println("BMP280 OK");
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   }
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-
+  
 
   //Pins
   pinMode(SOLENOID_VALVE_RELAY_PIN, OUTPUT);
--- frasers_controller/d_Loop.ino
+++ frasers_controller/d_Loop.ino
@@ -13,13 +13,20 @@ void loop() {
   if (rh_output > millis() - rh_start && rh_output > 500) digitalWrite(SOLENOID_VALVE_RELAY_PIN, HIGH); //NOTE THE >/<
   else digitalWrite(SOLENOID_VALVE_RELAY_PIN, LOW);
 
-  ih_PID.Compute();
-  if (millis() - ih_start > WINDOW_SIZE) ih_start = millis(); //time to shift the Relay Window (ih_start = millis() also works)
-  if (ih_output < millis() - ih_start) digitalWrite(IMMERSION_HEATER_RELAY_PIN, HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
-  else digitalWrite(IMMERSION_HEATER_RELAY_PIN, LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
+  
+  if(!sht.readSample() || (box_temperature == 0 && humidity == 0) || water_temperature < -5) {
+    digitalWrite(IMMERSION_HEATER_RELAY_PIN, HIGH); //turns off heater (active low)
+    digitalWrite(BOX_HEATER_RELAY_PIN, HIGH); //turns off heater (active low)
+  }
+  else {
+    ih_PID.Compute();
+    if (millis() - ih_start > WINDOW_SIZE) ih_start = millis(); //time to shift the Relay Window (ih_start = millis() also works)
+    if (ih_output < millis() - ih_start) digitalWrite(IMMERSION_HEATER_RELAY_PIN, HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR). 
+    else digitalWrite(IMMERSION_HEATER_RELAY_PIN, LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
 
-  bh_PID.Compute();
-  if (millis() - bh_start > WINDOW_SIZE) bh_start = millis(); //time to shift the Relay Window
-  if (bh_output < millis() - bh_start) digitalWrite(BOX_HEATER_RELAY_PIN, HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
-  else digitalWrite(BOX_HEATER_RELAY_PIN, LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
+    bh_PID.Compute();
+    if (millis() - bh_start > WINDOW_SIZE) bh_start = millis(); //time to shift the Relay Window
+    if (bh_output < millis() - bh_start) digitalWrite(BOX_HEATER_RELAY_PIN, HIGH); //window on time (INVERTED LOGIC SOLENOID VS SSR)
+    else digitalWrite(BOX_HEATER_RELAY_PIN, LOW); //window off time (INVERTED LOGIC SOLENOID VS SSR)
+  }
 }
